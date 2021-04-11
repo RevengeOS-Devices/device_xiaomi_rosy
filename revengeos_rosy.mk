f84ed2464bd7a68f9cc5dc2d7b2348c3347d9ce0
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-04-11 16:18:37 +0000

    rosy: revengeos bringup
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- revengeos_rosy.mk
+++ revengeos_rosy.mk
@@ -9,19 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# Pixel customization
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+# Inherit some common RevengeOS stuff
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
 # Inherit from rosy device
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_rosy
+PRODUCT_NAME := revengeos_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
@@ -29,9 +24,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 720
-TARGET_BOOTANIMATION_HALF_RES := true
 
-# GApps
-TARGET_GAPPS_ARCH := arm64
+
+# Inherit some common RevengeOS flags
+REVENGEOS_BUILDTYPE := OFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
